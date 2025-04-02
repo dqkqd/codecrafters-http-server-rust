0d1c7afdc482ad4d4afbf8c912d7bd8c06e6474a
Khanh Duong <dqkqdlot@gmail.com>
2025-04-03 03:36:35 +0900

    feat: response 200

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
-fn main() {
+fn main() -> std::io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -10,12 +10,15 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                stream.write_all(response.as_bytes())?
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
