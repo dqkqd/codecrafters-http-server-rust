313c6f936f4f657f03ca41fcd6eb9778166ade92
Khanh Duong <dqkqdlot@gmail.com>
2025-04-12 16:42:31 +0900

    feat: add response to spect

--- src/spec/mod.rs
+++ src/spec/mod.rs
@@ -1,3 +1,4 @@
 pub mod message;
 pub mod protocol;
 pub mod request;
+pub mod response;
--- src/spec/response.rs
+++ src/spec/response.rs
@@ -1,2 +1,39 @@
+
+use super::{
+    message::{MessageBody, MessageHeader},
+    protocol::HttpVersion,
+};
+
 #[derive(Debug)]
-pub(crate) struct StatusLine {}
+pub(crate) enum Status {
+    OK,
+    NotFound,
+}
+
+#[derive(Debug)]
+pub(crate) struct StatusLine {
+    pub http_version: HttpVersion,
+    pub status: Status,
+}
+
+#[derive(Debug)]
+pub struct Response {
+    pub(crate) status_line: StatusLine,
+    pub(crate) headers: Vec<MessageHeader>,
+    pub(crate) body: Option<MessageBody>,
+}
+
+impl Status {
+    pub fn code(&self) -> u16 {
+        match self {
+            Status::OK => 200,
+            Status::NotFound => 404,
+        }
+    }
+    pub fn reason_phrase(&self) -> &'static str {
+        match self {
+            Status::OK => "OK",
+            Status::NotFound => "Not Found",
+        }
+    }
+}
